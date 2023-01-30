bfe1cf3d274253baf3ab04ee3c886230452c44ba
duynghiadev <duynghia22302@gmail.com>
2023-01-31 04:52:30 +0700

    bai 32: Lab Filter Todo

--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -25,6 +25,7 @@ function TodoFeature(props) {
   ];
 
   const [todoList, setTodoList] = useState(initTodoList);
+  const [filteredStatus, setFilteredStatus] = useState('all');
 
   const handleTodoClick = (todo, idx) => {
     // clone current array to the new one
@@ -42,10 +43,30 @@ function TodoFeature(props) {
     setTodoList(newTodoList);
   };
 
+  const handleShowAllClick = () => {
+    setFilteredStatus('all');
+  };
+
+  const handleShowCompletedClick = () => {
+    setFilteredStatus('completed');
+  };
+
+  const handleShowNewClick = () => {
+    setFilteredStatus('new');
+  };
+
+  const renderedTodoList = todoList.filter((todo) => filteredStatus === 'all' || filteredStatus === todo.status);
+
   return (
     <div>
       <h3>Todo List</h3>
-      <TodoList todoList={todoList} onTodoClick={handleTodoClick} />
+      <TodoList todoList={renderedTodoList} onTodoClick={handleTodoClick} />
+
+      <div>
+        <button onClick={handleShowAllClick}>Show All</button>
+        <button onClick={handleShowCompletedClick}>Show Completed</button>
+        <button onClick={handleShowNewClick}>Show New</button>
+      </div>
     </div>
   );
 }
