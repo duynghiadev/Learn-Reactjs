5f98bf650b4980f215ba5ff92203e33ed3d27e84
duynghiadev <duynghia22302@gmail.com>
2023-02-01 08:27:01 +0700

    call api with useEffect hooks

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import ChangeForm from 'components/ChangeForm';
 import ChangeList from 'components/ChangeList';
 import ColorBox from 'components/ColorBox';
 import Counter from 'components/Counter';
+import PostList from 'components/PostList';
 import AlbumFeature from 'features/Album';
 import TodoFeature from 'features/Todo';
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import BoxClick from 'react hook/example1';
 import DailyList from 'react hook/example2';
 
@@ -16,6 +18,31 @@ function App() {
     { id: 2, title: 'We love Easy Frontend! 🥰' },
     { id: 3, title: 'They love Easy Frontend! 🚀' },
   ]);
+
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const requestUrl = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+        console.log({ responseJSON });
+
+        const { data } = responseJSON;
+        setPostList(data);
+      } catch (error) {
+        console.log('Failed to fetch post list: ', error.message);
+      }
+    }
+
+    console.log('POST list effect');
+    fetchPostList();
+  }, []);
+
+  useEffect(() => {
+    console.log('TODO list effect');
+  });
 
   function handleTodoClick(todo) {
     console.log(todo);
@@ -44,6 +71,7 @@ function App() {
       {/* This is content for the Change Color and Change Form */}
       <ChangeForm onSubmit={handleTodoFormSubmit} />
       <ChangeList todos={todoList} onTodoClick={handleTodoClick} />
+      <PostList posts={postList} />
 
       {/* <ChangeColor /> */}
       {/* <BoxClick /> */}
