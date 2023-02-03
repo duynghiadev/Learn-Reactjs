98e7c69838520978de996c039042a626dcf70343
duynghiadev <duynghia22302@gmail.com>
2023-02-04 04:52:47 +0700

    bài 49: Sự khác biệt giữa Link và NavLink

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import HomePage from 'pages/HomePage';
-import { Route } from 'react-router-dom';
+import { Link, NavLink, Route } from 'react-router-dom';
 import TodoFeature from 'features/Todo';
 import AlbumFeature from 'features/Album';
 
@@ -8,9 +8,26 @@ function App() {
   return (
     <div className="app">
       {/* <HomePage /> */}
-      To do feature
+      Header
+      <p>
+        <Link to="/todos">Todos</Link>
+      </p>
+      <p>
+        <Link to="/albums">Albums</Link>
+      </p>
+      <p>
+        <NavLink to="/todos" activeClassName="active-todo">
+          Todos
+        </NavLink>
+      </p>
+      <p>
+        <NavLink to="/albums" activeClassName="active-albums">
+          Albums
+        </NavLink>
+      </p>
       <Route path="/todos" component={TodoFeature} />
       <Route path="/albums" component={AlbumFeature} />
+      Footer
     </div>
   );
 }
