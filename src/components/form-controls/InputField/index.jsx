f871ef4dbdd323b36f6c137802fdf21a65ab0060
duynghiadev <duynghia22302@gmail.com>
2023-02-07 03:37:32 +0700

    bài 71: Tạo custom field: InputField

--- src/components/form-controls/InputField/index.jsx
+++ src/components/form-controls/InputField/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { TextField } from '@material-ui/core';
+import { Controller } from 'react-hook-form';
 
-InputField.propTypes = {};
+InputField.propTypes = {
+  form: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  disabled: PropTypes.bool,
+};
 
 function InputField(props) {
+  const { form, name, label, disabled } = props;
+
   return (
-    <div>
+    <Controller name={name} control={form.control} as={TextField} fullWidth label={label} disabled={disabled}>
       <TextField fullWidth />
-    </div>
+    </Controller>
   );
 }
 
