f6cfc612fdb2b8581e8c51dd53b3d41e0b7f42f2
duynghiadev <duynghia22302@gmail.com>
2023-03-08 04:50:44 +0700

    87. SCSS + BEM

--- src/features/Album/components/Album/index.jsx
+++ src/features/Album/components/Album/index.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.scss';
 
+Album.propTypes = {
+  album: PropTypes.object.isRequired,
+};
+
 function Album(props) {
   const { album } = props;
 
   return (
-    <div className="album">
-      <div className="album_thumbnail">
-        <img src={album.thumbnail} alt={album.name} />
+    <div className="album album--active album--special">
+      <div className="album__thumbnail album__thumbnail--active">
+        <img className="album__image" src={album.thumbnail} alt={album.name} />
 
         {/* Other control */}
       </div>
-      <p className="album_name">{album.name}</p>
+      <p className="album__name">{album.name}</p>
     </div>
   );
 }
-
-Album.propTypes = {
-  album: PropTypes.object.isRequired,
-};
 
 export default Album;
