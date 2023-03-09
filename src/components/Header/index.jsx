a28ce5eac572aa4e052ebc8f5cf634945bd8c9b3
duynghiadev <duynghia22302@gmail.com>
2023-03-10 05:56:43 +0700

    109. Thông báo thành công

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -74,7 +74,7 @@ export default function Header() {
         aria-labelledby="form-dialog-title"
       >
         <DialogContent>
-          <Register />
+          <Register closeDialog={handleClose} />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
--- src/features/Auth/components/Register/index.jsx
+++ src/features/Auth/components/Register/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import RegisterForm from '../RegisterForm';
 import { useDispatch } from 'react-redux';
 import { register } from 'features/Auth/userSlice';
 import { unwrapResult } from '@reduxjs/toolkit';
+import { useSnackbar } from 'notistack';
+
+Register.propTypes = {
+  closeDialog: PropTypes.func,
+};
 
 function Register(props) {
   const dispatch = useDispatch();
+  const { enqueueSnackbar } = useSnackbar();
 
   const handleSubmit = async (values) => {
     try {
@@ -14,10 +21,16 @@ function Register(props) {
 
       const action = register(values);
       const resultAction = await dispatch(action);
-      const user = unwrapResult(resultAction);
+      unwrapResult(resultAction);
+
+      // close dialog
+      const { closeDialog } = props;
+      if (closeDialog) {
+        closeDialog();
+      }
 
       // do something here on register successfully
-      console.log('New user: ', user);
+      enqueueSnackbar('Register successfully!!! ✅', { variant: 'success' });
     } catch (error) {
       console.log('Failed to register: ', error);
     }
@@ -29,7 +42,5 @@ function Register(props) {
     </div>
   );
 }
-
-Register.propTypes = {};
 
 export default Register;
