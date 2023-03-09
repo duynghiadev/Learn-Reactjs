4bd854864ce740518bbb1b32f94c5444d94bc351
duynghiadev <duynghia22302@gmail.com>
2023-03-10 03:48:39 +0700

    104. Tạo async action: register()

--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -1,4 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import userApi from 'api/useApi';
+
+const register = createAsyncThunk('users/register', async (payload) => {
+  // call API to register
+  const data = await userApi.register(payload);
+
+  // save data to local storage
+  localStorage.setItem('access_token', data.jwt);
+  localStorage.setItem('user', JSON.stringify(data.user));
+
+  // return user data
+  return data.user;
+});
 
 const userSlice = createSlice({
   name: 'user',
@@ -7,6 +20,11 @@ const userSlice = createSlice({
     settings: {},
   },
   reducers: {},
+  extraReducers: {
+    [register.fulfilled]: (state, action) => {
+      state.current = action.payload;
+    },
+  },
 });
 
 const { reducer } = userSlice;
