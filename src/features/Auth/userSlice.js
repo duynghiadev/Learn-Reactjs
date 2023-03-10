b158897874ea65bf354a3b9a67eef521c3c21865
duynghiadev <duynghia22302@gmail.com>
2023-03-10 08:45:01 +0700

    112. Setup API đăng nhập

--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -1,9 +1,21 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import userApi from 'api/useApi';
+import userApi from 'api/userApi';
 
 export const register = createAsyncThunk('users/register', async (payload) => {
   // call API to register
   const data = await userApi.register(payload);
+
+  // save data to local storage
+  localStorage.setItem('access_token', data.jwt);
+  localStorage.setItem('user', JSON.stringify(data.user));
+
+  // return user data
+  return data.user;
+});
+
+export const login = createAsyncThunk('users/login', async (payload) => {
+  // call API to login
+  const data = await userApi.login(payload);
 
   // save data to local storage
   localStorage.setItem('access_token', data.jwt);
@@ -22,6 +34,10 @@ const userSlice = createSlice({
   reducers: {},
   extraReducers: {
     [register.fulfilled]: (state, action) => {
+      state.current = action.payload;
+    },
+
+    [login.fulfilled]: (state, action) => {
       state.current = action.payload;
     },
   },
