caecaa06ef91fb46cf0486e23ba6dbf096047718
duynghiadev <duynghia22302@gmail.com>
2023-03-11 10:39:05 +0700

    123. Setup layout cho Product Listing

--- src/features/Product/pages/ListPage.jsx
+++ src/features/Product/pages/ListPage.jsx
@@ -1,7 +1,35 @@
+import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
 import React from 'react';
 
+const useStyles = makeStyles((theme) => ({
+  root: {},
+
+  left: {
+    width: '250px',
+  },
+
+  right: {
+    flex: '1 1 auto',
+  },
+}));
+
 function ListPage(props) {
-  return <div>Product Listing Page</div>;
+  const classes = useStyles();
+
+  return (
+    <Box>
+      <Container>
+        <Grid container spacing={1}>
+          <Grid item className={classes.left}>
+            <Paper elevation={0}>Left Column</Paper>
+          </Grid>
+          <Grid item className={classes.right}>
+            <Paper elevation={0}>Right Column</Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
 }
 
 export default ListPage;
