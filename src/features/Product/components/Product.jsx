24c31f1fab411a6eeae12947d7c65d299fba7c27
duynghiadev <duynghia22302@gmail.com>
2023-03-11 17:15:25 +0700

    127. Xử lý thumbnail cho sản phẩm

--- src/features/Product/components/Product.jsx
+++ src/features/Product/components/Product.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Box, Typography } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
+import { STATIC_HOST, THUMBNAIL_PLACEHOLDER } from 'constants/index';
 
 Product.propTypes = {
   product: PropTypes.object,
 };
 
 function Product({ product }) {
+  const thumbnailUrl = product.thumbnail ? `${STATIC_HOST}${product.thumbnail?.url}` : THUMBNAIL_PLACEHOLDER;
+
   return (
     <Box padding={1}>
-      <Skeleton variant="react" width="100%" height={118} />
+      <Box padding={1}>
+        <img src={thumbnailUrl} alt={product.name} width="100%" />
+      </Box>
 
       <Typography variant="body2">{product.name}</Typography>
       <Typography variant="body2">
