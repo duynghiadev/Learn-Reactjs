6237ebdd9b4e741aff4c6f9d025ef8d90f00a056
duynghiadev <duynghia22302@gmail.com>
2023-03-12 09:07:11 +0700

    137. Lọc theo dịch vụ sản phẩm

--- src/features/Product/components/ProductFilters.jsx
+++ src/features/Product/components/ProductFilters.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Box } from '@material-ui/core';
 import FilterByCategory from './Filters/FilterByCategory';
 import FilterByPrice from './Filters/FilterByPrice';
+import FilterByService from './Filters/FilterByService';
 
 ProductFilters.propTypes = {
   filters: PropTypes.object.isRequired,
@@ -19,7 +20,7 @@ function ProductFilters({ filters, onChange }) {
     onChange(newFilters);
   };
 
-  const handlePriceChange = (values) => {
+  const handleChange = (values) => {
     if (onChange) {
       onChange(values);
     }
@@ -28,7 +29,8 @@ function ProductFilters({ filters, onChange }) {
   return (
     <Box>
       <FilterByCategory onChange={handleCategoryChange} />
-      <FilterByPrice onChange={handlePriceChange} />
+      <FilterByPrice onChange={handleChange} />
+      <FilterByService filters={filters} onChange={handleChange} />
     </Box>
   );
 }
