48d1ba33e556244758c70f7467f82032482c9156
duynghiadev <duynghia22302@gmail.com>
2023-03-16 14:11:44 +0700

    157. Tạo actions cho cart items

--- src/features/Cart/cartSlice.js
+++ src/features/Cart/cartSlice.js
@@ -14,9 +14,36 @@ const cartSlice = createSlice({
     hideMiniCart(state) {
       state.showMiniCart(false);
     },
+
+    addToCart(state, action) {
+      // newItem = {id, product, quantity}
+      const newItem = action.payload;
+      const index = state.cartItems.findIndex((x) => x.id === newItem.id);
+      if (index >= 0) {
+        // increase quantity
+        state.cartItems[index].quantity += newItem.quantity;
+      } else {
+        // add to cart
+        state.cartItems.push(newItem);
+      }
+    },
+
+    setQuantity(state, action) {
+      const { id, quantity } = action.payload;
+      // check if product is available in cart
+      const index = state.cartItems.findIndex((x) => x.id === id);
+      if (index >= 0) {
+        state.cartItems[index].quantity = quantity;
+      }
+    },
+
+    removeFromCart(state, action) {
+      const idNeedToRemove = action.payload;
+      state.cartItems = state.cartItems.filter((x) => x.id !== idNeedToRemove);
+    },
   },
 });
 
-const { actions, reducers } = cartSlice;
+const { actions, reducer } = cartSlice;
 export const { showMiniCart, hideMiniCart } = actions; // name export
-export default reducers; // default export
+export default reducer; // default export
