9245a418951226eb4fe83df67a063d1966804a84
duynghiadev <duynghia22302@gmail.com>
2023-03-16 08:40:53 +0700

    150. Code QuantityField

--- src/features/Product/components/AddToCartForm.jsx
+++ src/features/Product/components/AddToCartForm.jsx
@@ -5,6 +5,7 @@ import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import InputField from 'components/form-controls/InputField';
 import { Button } from '@material-ui/core';
+import QuantityField from 'components/form-controls/QuantityField';
 
 AddToCartForm.propTypes = {
   onSubmit: PropTypes.func,
@@ -12,7 +13,11 @@ AddToCartForm.propTypes = {
 
 function AddToCartForm({ onSubmit = null }) {
   const schema = yup.object().shape({
-    quantity: yup.number().required('Please enter quantity').min(1, 'Please enter at least 1'),
+    quantity: yup
+      .number()
+      .required('Please enter quantity')
+      .min(1, 'Minimum value is 1')
+      .typeError('Please enter a number'),
   });
 
   const form = useForm({
@@ -30,10 +35,10 @@ function AddToCartForm({ onSubmit = null }) {
 
   return (
     <form onSubmit={form.handleSubmit(handleSubmit)}>
-      <InputField name="quantity" label="Quantity" form={form} />
+      <QuantityField name="quantity" label="Quantity" form={form} />
 
-      <Button type="submit" variant="contained" color="primary" fullWidth size="large">
-        Buy
+      <Button type="submit" variant="contained" color="primary" style={{ width: '250px' }} size="large">
+        Add To Cart
       </Button>
     </form>
   );
