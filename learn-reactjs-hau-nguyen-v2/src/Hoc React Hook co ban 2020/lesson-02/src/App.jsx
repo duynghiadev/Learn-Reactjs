6ae465a03efe23747832396999c2c37629ca8e64
duynghiadev <duynghia22302@gmail.com>
2024-01-16 16:28:43 +0700

    React hooks: 02 - useState() và điều cần lưu ý 😊 (2020)

--- learn-reactjs-hau-nguyen-v2/src/Hoc React Hook co ban 2020/lesson-02/src/App.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Hoc React Hook co ban 2020/lesson-02/src/App.jsx
@@ -1,9 +1,15 @@
 import './App.scss'
+import ColorBox from './components/ColorBox'
+import ColorBox2 from './components/ColorBox2'
+import TodoList from './components/TodoList'
 
 function App() {
   return (
     <div className='app'>
       <h2>Welcome to React Hook</h2>
+      <ColorBox />
+      <ColorBox2 />
+      <TodoList />
     </div>
   )
 }
---
+++ learn-reactjs-hau-nguyen-v2/src/Hoc React Hook co ban 2020/lesson-02/src/components/TodoList.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+function TodoList() {
+  const [todoList, setTodoList] = useState(['love', 'easy', 'frontend'])
+
+  function removeTodo(index) {
+    // remember to clone into a new array
+    const newTodoList = [...todoList]
+
+    newTodoList.splice(index, 1)
+    setTodoList(newTodoList)
+  }
+
+  return (
+    <ul className='todo-list'>
+      {todoList.map((todo, index) => (
+        <li key={todo.id} onClick={() => removeTodo(index)}>
+          {todo}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default TodoList
