d3c704feb0146b086219a4f94bfb1e0b7f3bb4fd
duynghiadev <duynghia22302@gmail.com>
2024-01-24 21:13:36 +0700

    Add project learn react hook 02

---
+++ learn-reactjs-hau-nguyen-v2/src/Hoc React Hook co ban 2020/Self Study React Hook/React Hook 02/src/components/useState/UseState.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+const UseState = () => {
+  const [counter, setCounter] = useState(0)
+
+  let increment = () => {
+    setCounter(counter + 1)
+  }
+
+  return (
+    <div id='usestate'>
+      <h1>{counter}</h1>
+      <button onClick={increment}>Increment</button>
+    </div>
+  )
+}
+
+export default UseState
