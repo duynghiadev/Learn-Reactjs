6de03c041848ba581369c718a3b2641aa8c2845c
duynghiadev <duynghia22302@gmail.com>
2024-01-25 13:07:30 +0700

    Import UseState component in App and modify button label for clarity.

--- learn-reactjs-hau-nguyen-v2/src/Hoc React Hook co ban 2020/Self Study React Hook/React Hook 02/src/components/useState/UseState.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Hoc React Hook co ban 2020/Self Study React Hook/React Hook 02/src/components/useState/UseState.jsx
@@ -3,14 +3,15 @@ import { useState } from 'react'
 const UseState = () => {
   const [counter, setCounter] = useState(0)
 
-  let increment = () => {
+  const handleIncrement = () => {
     setCounter(counter + 1)
   }
 
   return (
     <div id='usestate'>
+      <strong>This is a useState hook</strong>
       <h1>{counter}</h1>
-      <button onClick={increment}>Increment</button>
+      <button onClick={handleIncrement}>Click</button>
     </div>
   )
 }
