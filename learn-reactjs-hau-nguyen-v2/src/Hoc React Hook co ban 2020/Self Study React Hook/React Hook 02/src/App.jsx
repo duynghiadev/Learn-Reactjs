13f6cfa1313658bfaed32b2ffdea9ecee7a28e69
duynghiadev <duynghia22302@gmail.com>
2024-01-25 13:51:11 +0700

    Add useReducer component to manage state for better app functionality.

--- learn-reactjs-hau-nguyen-v2/src/Hoc React Hook co ban 2020/Self Study React Hook/React Hook 02/src/App.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Hoc React Hook co ban 2020/Self Study React Hook/React Hook 02/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import UseReducer from './components/useReducer/UseReducer'
 import UseState from './components/useState/UseState'
 
 function App() {
   return (
     <div>
       <UseState />
+      <hr />
+      <UseReducer />
       <hr />
     </div>
   )
