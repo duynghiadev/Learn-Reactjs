529c4cf6df8395b3a10cc4777af3194d915f94ac
duynghiadev <duynghia22302@gmail.com>
2024-02-09 12:41:52 +0700

    25. Props type checking

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/App.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div>
       <h1>Hello React Udemy Hau Nguyen 2020</h1>
+      <h2>Hello, my name is Duy Nghia. Currently, i'm studying ReactJS</h2>
     </div>
   )
 }
