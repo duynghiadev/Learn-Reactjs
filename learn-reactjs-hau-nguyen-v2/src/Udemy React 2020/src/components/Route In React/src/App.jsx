362891a02dd06930f047d454085fa7ae952b6909
duynghiadev <duynghia22302@gmail.com>
2024-02-29 04:21:00 +0700

    52. Tạo redirect rule với Redirect

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/App.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/App.jsx
@@ -1,4 +1,4 @@
-import { Switch, NavLink, Route } from 'react-router-dom'
+import { Switch, NavLink, Route, Redirect } from 'react-router-dom'
 import './App.css'
 import TodoFeature from './features/Todo'
 import AlbumFeature from './features/Album'
@@ -14,6 +14,8 @@ function App() {
         <NavLink to='/albums'>Albums</NavLink>
       </p>
       <Switch>
+        <Redirect from='/home' to='/' exact />
+        <Redirect from='/post-list/:postId' to='/post/:postId' exact />
         <Route path='/' component={TodoFeature} exact />
         <Route path='/todos' component={TodoFeature} />
         <Route path='/albums' component={AlbumFeature} />
