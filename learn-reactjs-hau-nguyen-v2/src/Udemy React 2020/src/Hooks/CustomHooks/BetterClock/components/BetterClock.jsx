7422a8e8e689ec2d9c2d5aea099e708401ba84a0
duynghiadev <duynghia22302@gmail.com>
2024-02-28 15:48:28 +0700

    feat: add className and write scss for project Clock in Custom hooks

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/Hooks/CustomHooks/BetterClock/components/BetterClock.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/Hooks/CustomHooks/BetterClock/components/BetterClock.jsx
@@ -1,12 +1,24 @@
+import React, { useState } from 'react'
 import useClock from '../hooks/useClock'
 import './BetterClock.scss'
 
 const BetterClock = () => {
   const { timeString } = useClock()
+  const [isClockVisible, setIsClockVisible] = useState(true)
+
+  const toggleClockVisibility = () => {
+    setIsClockVisible(!isClockVisible)
+  }
 
   return (
     <div className='better-clock'>
-      <p className='better-clock__time'>{timeString}</p>
+      {isClockVisible && <p className='better-clock__time'>{timeString}</p>}
+      <button
+        className={`better-clock__toggle ${isClockVisible ? 'visible' : 'hidden'}`}
+        onClick={toggleClockVisibility}
+      >
+        {isClockVisible ? 'Hide Clock' : 'Show Clock'}
+      </button>
     </div>
   )
 }
