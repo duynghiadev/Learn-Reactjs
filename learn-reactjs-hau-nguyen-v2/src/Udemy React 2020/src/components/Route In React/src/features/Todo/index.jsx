49d5704893f12ca8074cbaa435b4562e920214bf
duynghiadev <duynghia22302@gmail.com>
2024-02-29 04:45:37 +0700

    54. Nested routing

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Todo/index.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Todo/index.jsx
@@ -1,71 +1,20 @@
 import PropTypes from 'prop-types'
 import TodoList from './components/TodoList/TodoList'
 import { useState } from 'react'
+import { Switch, Route, useRouteMatch } from 'react-router-dom'
+import ListPage from './pages/ListPage'
+import DetailPage from './pages/DetailPage'
 
 const TodoFeature = (props) => {
-  const initTodoList = [
-    {
-      id: 1,
-      title: 'Eat',
-      status: 'new'
-    },
-    {
-      id: 2,
-      title: 'Sleep',
-      status: 'completed'
-    },
-    {
-      id: 3,
-      title: 'Code',
-      status: 'new'
-    }
-  ]
-
-  const [todoList, setTodoList] = useState(initTodoList)
-  const [filteredStatus, setFilteredStatus] = useState('all')
-
-  const handleTodoClick = (todo, idx) => {
-    console.log(todo, idx)
-
-    // clone current array to the new one
-    const newTodoList = [...todoList]
-
-    // toggle state
-    const newTodo = {
-      ...newTodoList[idx],
-      status: newTodoList[idx].status === 'new' ? 'completed' : 'new'
-    }
-    newTodoList[idx] = newTodo
-
-    // update todo list
-    setTodoList(newTodoList)
-  }
-
-  const handleShowAllClick = () => {
-    setFilteredStatus('all')
-  }
-
-  const handleShowCompletedClick = () => {
-    setFilteredStatus('completed')
-  }
-
-  const handleShowNewClick = () => {
-    setFilteredStatus('new')
-  }
-
-  const renderedTodoList = todoList.filter(
-    (todo) => filteredStatus === 'all' || filteredStatus === todo.status
-  )
-  console.log(renderedTodoList)
+  const match = useRouteMatch()
 
   return (
     <div>
-      <h3>To Do List</h3>
-      <TodoList todoList={renderedTodoList} onTodoClick={handleTodoClick} />
-
-      <button onClick={handleShowAllClick}>Show All</button>
-      <button onClick={handleShowCompletedClick}>Show Completed</button>
-      <button onClick={handleShowNewClick}>Show New</button>
+      TODO SHARED UI
+      <Switch>
+        <Route path={match.path} component={ListPage} exact />
+        <Route path={`${match.path}/:todoId`} component={DetailPage} />
+      </Switch>
     </div>
   )
 }
---
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Todo/pages/DetailPage/index.jsx
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types'
+
+const DetailPage = (props) => {
+  return <div>Todo Detail Page</div>
+}
+
+DetailPage.propTypes = {}
+
+export default DetailPage
