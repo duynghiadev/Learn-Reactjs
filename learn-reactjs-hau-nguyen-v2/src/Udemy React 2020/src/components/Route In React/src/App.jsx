aacf78f81dee32d18a70ba2035327ebd72db1ea6
duynghiadev <duynghia22302@gmail.com>
2024-02-29 03:51:27 +0700

    49. Sự khác biệt giữa Link và NavLink

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/App.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom'
+import { Link, NavLink, Route } from 'react-router-dom'
 import './App.css'
 import TodoFeature from './features/Todo'
 import AlbumFeature from './features/Album'
@@ -7,6 +7,18 @@ function App() {
   return (
     <div className='App'>
       Header
+      <p>
+        <Link to='/todos'>Todos</Link>
+      </p>
+      <p>
+        <Link to='/albums'>Albums</Link>
+      </p>
+      <p>
+        <NavLink to='/todos'>Todos</NavLink>
+      </p>
+      <p>
+        <NavLink to='/albums'>Albums</NavLink>
+      </p>
       <Route path='/todos' component={TodoFeature} />
       <Route path='/albums' component={AlbumFeature} />
       Footer
