9c8daa0e3d49c1e0fd40ffd2c59ec5daa63a84a7
duynghiadev <duynghia22302@gmail.com>
2024-02-29 08:51:28 +0700

    64. Demo gọi API để lấy dữ liệu

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/App.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/App.jsx
@@ -3,8 +3,22 @@ import './App.css'
 import TodoFeature from './features/Todo'
 import AlbumFeature from './features/Album'
 import NotFound from './components/NotFound'
+import { useEffect } from 'react'
+import productApi from './api/productApi'
 
 function App() {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const params = {
+        _limit: 10
+      }
+      const productList = await productApi.getAll(params)
+      console.log(productList)
+    }
+
+    fetchProducts()
+  }, [])
+
   return (
     <div className='App'>
       Header
