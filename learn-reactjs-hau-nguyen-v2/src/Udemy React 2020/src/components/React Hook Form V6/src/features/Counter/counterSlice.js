3e166a8178debda1b185e717a7a0e5750ee66bc3
duynghiadev <duynghia22302@gmail.com>
2024-02-29 14:57:03 +0700

    fix: completed fix bug replace require to import from '...'

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/React Hook Form V6/src/features/Counter/counterSlice.js
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/React Hook Form V6/src/features/Counter/counterSlice.js
@@ -1,17 +1,17 @@
-const { createSlice } = require('@reduxjs/toolkit');
+import { createSlice } from '@reduxjs/toolkit';
 
 const counterSlice = createSlice({
-    name: 'counter',
-    initialState: 0,
-    reducers: {
-        increase(state) {
-            return state + 1;
-        },
-        decrease(state) {
-            return state - 1; 
-        }
-    }
-})
+  name: 'counter',
+  initialState: 0,
+  reducers: {
+    increase(state) {
+      return state + 1;
+    },
+    decrease(state) {
+      return state - 1;
+    },
+  },
+});
 
 const { actions, reducer } = counterSlice;
 export const { increase, decrease } = actions;
