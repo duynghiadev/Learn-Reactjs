867c46b399e4198a100f6ea950967c32dbb30ab8
duynghiadev <duynghia22302@gmail.com>
2024-02-29 17:07:32 +0700

    71. Tạo custom field: InputField

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/components/form-controls/InputField/index.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/components/form-controls/InputField/index.jsx
@@ -1,11 +1,22 @@
 import { TextField } from '@material-ui/core'
+import PropTypes from 'prop-types'
+import { Controller } from 'react-hook-form'
 
-const InputField = () => {
+const InputField = (props) => {
+  const { form, name, label, disabled } = props
+
   return (
-    <div>
-      <TextField fullWidth />
-    </div>
+    <>
+      <Controller name={name} control={form.control} as={TextField} fullWidth label={label} disabled={disabled} />
+    </>
   )
+}
+
+InputField.propTypes = {
+  form: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  disabled: PropTypes.bool
 }
 
 export default InputField
