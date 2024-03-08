afd1f86cf9094c73efe1ad4b573526ad51db5791
duynghiadev <duynghia22302@gmail.com>
2024-03-08 15:27:52 +0700

    85. Inline styles

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/redux/section13-udemy/src/App.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/redux/section13-udemy/src/App.jsx
@@ -2,11 +2,14 @@ import './App.css'
 import CounterFeature from './Counter/index.jsx'
 
 function App() {
+  const color = 'goldenrod'
+  const backgroundUrl = 'https://images.unsplash.com/photo-1709642717827-9621f2a978a1?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8'
+
   return (
-    <>
+    <div className="App" style={{ color: color, backgroundImage: `url('${backgroundUrl}')` }}>
       <h2>This is section 13, I learned on udemy by HauNguyen</h2>
       <CounterFeature />
-    </>
+    </div>
   )
 }
 
