c5c38fd5d96a43ce709f0727afb80559eea35ee9
duynghiadev <duynghia22302@gmail.com>
2024-03-10 08:15:51 +0700

    115. Tạo hằng số cho dự án

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Auth/userSlice.js
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Auth/userSlice.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import userApi from '../../api/userApi.jsx'
+import StorageKeys from '../../constants/storage-keys.js'
 
 export const register = createAsyncThunk(
   'user/register',
@@ -7,8 +8,8 @@ export const register = createAsyncThunk(
     const data = await userApi.register(payload)
 
     // save data to local storage
-    localStorage.setItem('access_token', data.jwt)
-    localStorage.setItem('user', JSON.stringify(data.user))
+    localStorage.setItem(StorageKeys.TOKEN, data.jwt)
+    localStorage.setItem(StorageKeys.USER, JSON.stringify(data.user))
 
     // return user data
     return data.user
@@ -20,8 +21,8 @@ export const login = createAsyncThunk(
     const data = await userApi.login(payload)
 
     // save data to local storage
-    localStorage.setItem('access_token', data.jwt)
-    localStorage.setItem('user', JSON.stringify(data.user))
+    localStorage.setItem(StorageKeys.TOKEN, data.jwt)
+    localStorage.setItem(StorageKeys.USER, JSON.stringify(data.user))
 
     // return user data
     return data.user
