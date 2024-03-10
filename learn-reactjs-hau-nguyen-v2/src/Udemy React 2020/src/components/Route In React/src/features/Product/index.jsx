05f73ce221ae9d63d9ec4da1fa8e5ea4b2852801
duynghiadev <duynghia22302@gmail.com>
2024-03-10 13:51:42 +0700

    123. Setup layout cho Product Listing

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Product/index.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Product/index.jsx
@@ -1,3 +1,4 @@
+import { Box } from '@material-ui/core'
 import { Route, Switch, useRouteMatch } from 'react-router-dom'
 import ListPage from './pages/ListPage.jsx'
 
@@ -5,12 +6,11 @@ function ProductFeature(props) {
   const match = useRouteMatch()
 
   return (
-    <div>
-      Product Feature
+    <Box pt={4}>
       <Switch>
         <Route path={match.url} exact component={ListPage} />
       </Switch>
-    </div>
+    </Box>
   )
 }
 
--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Product/pages/ListPage.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Product/pages/ListPage.jsx
@@ -1,8 +1,39 @@
+import { Box, Container, Grid, Paper } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+
+  left: {
+    width: '250px'
+  },
+
+  right: {
+    flex: '1 1 auto'
+  }
+}))
+
 function ListPage(props) {
+  const classes = useStyles()
+
   return (
-    <div>
-      Product Listing Page
-    </div>
+    <Box>
+      <Container>
+        <Grid container spacing={1}>
+          <Grid item className={classes.left}>
+            <Paper elevation={0}>
+              Left column
+            </Paper>
+          </Grid>
+          
+          <Grid item className={classes.right}>
+            <Paper elevation={0}>
+              Right column
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   )
 }
 
