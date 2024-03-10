784050c66aa71966f7164f8f60e8aab0ccf1aa9a
duynghiadev <duynghia22302@gmail.com>
2024-03-10 15:51:00 +0700

    127. Xử lý thumbnail cho sản phẩm

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Product/components/Product.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Product/components/Product.jsx
@@ -1,11 +1,17 @@
 import { Box, Typography } from '@material-ui/core'
-import { Skeleton } from '@material-ui/lab'
 import PropTypes from 'prop-types'
+import { STATIC_HOST, THUMBNAIL_PLACEHOLDER } from '../../../constants'
 
 const Product = ({ product }) => {
+  const thumbnailUrl = product.thumbnail
+    ? `${STATIC_HOST}${product.thumbnail?.url}`
+    : `${THUMBNAIL_PLACEHOLDER}`
+
   return (
     <Box padding={1}>
-      <Skeleton variant='rect' width='100%' height={118} />
+      <Box padding={1}>
+        <img src={thumbnailUrl} alt={product.name} width='100%' />
+      </Box>
 
       <Typography variant='body2'>{product.name}</Typography>
       <Typography variant='body2'>
