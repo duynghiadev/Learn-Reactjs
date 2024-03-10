acb07285b2b0bf0d253a87befa6b545387d93012
duynghiadev <duynghia22302@gmail.com>
2024-03-10 18:52:10 +0700

    135. Lọc theo khoảng giá

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Product/components/Filters/FilterByPrice.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Product/components/Filters/FilterByPrice.jsx
@@ -1,5 +1,50 @@
-const FilterByPrice = (props) => {
-  return <div>Filter By Price</div>
+import { Box, Button, TextField, Typography } from '@material-ui/core'
+import PropTypes from 'prop-types'
+import { useState } from 'react'
+
+const FilterByPrice = ({ onChange }) => {
+  const [values, setValues] = useState({
+    salePrice_gte: 0,
+    salePrice_lte: 0
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = () => {
+    console.log(values)
+    if (onChange) onChange(values)
+
+    setValues({
+      salePrice_gte: 0,
+      salePrice_lte: 0
+    })
+  }
+
+  return (
+    <Box>
+      <Typography variant='subtitle2'>GIÁ</Typography>
+
+      <Box>
+        <TextField name='salePrice_gte' value={values.salePrice_gte} onChange={handleChange} />
+        <span>-</span>
+        <TextField name='salePrice_lte' value={values.salePrice_lte} onChange={handleChange} />
+      </Box>
+
+      <Button variant='outlined' color='primary' onClick={handleSubmit}>
+        Áp Dụng
+      </Button>
+    </Box>
+  )
+}
+
+FilterByPrice.propTypes = {
+  onChange: PropTypes.func
 }
 
 export default FilterByPrice
--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Product/components/ProductFilters.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Product/components/ProductFilters.jsx
@@ -1,21 +1,27 @@
 import { Box } from '@material-ui/core'
 import PropTypes from 'prop-types'
 import FilterByCategory from './Filters/FilterByCategory'
+import FilterByPrice from './Filters/FilterByPrice'
 
 const ProductFilters = ({ filters, onChange }) => {
   const handleCategoryChange = (newCategoryId) => {
     if (!onChange) return
 
     const newFilters = {
-      ...filters,
       'category.id': newCategoryId
     }
     onChange(newFilters)
   }
 
+  const handleByChange = (values) => {
+    console.log(values)
+    if (onChange) onChange(values)
+  }
+
   return (
     <Box>
       <FilterByCategory onChange={handleCategoryChange} />
+      <FilterByPrice onChange={handleByChange} />
     </Box>
   )
 }
