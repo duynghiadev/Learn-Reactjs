c1b95e849f885804ccb938aa3ef069b3226b531d
duynghiadev <duynghia22302@gmail.com>
2024-03-11 11:18:52 +0700

    136. Giao diện lọc khoảng giá

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Product/components/Filters/FilterByPrice.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Product/components/Filters/FilterByPrice.jsx
@@ -1,8 +1,29 @@
-import { Box, Button, TextField, Typography } from '@material-ui/core'
+import { Box, Button, TextField, Typography, makeStyles } from '@material-ui/core'
 import PropTypes from 'prop-types'
 import { useState } from 'react'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+    borderTop: `1px solid ${theme.palette.grey[300]}`
+  },
+
+  range: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    alignItems: 'center',
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+
+    '& > span': {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1)
+    }
+  }
+}))
+
 const FilterByPrice = ({ onChange }) => {
+  const classes = useStyles()
   const [values, setValues] = useState({
     salePrice_gte: 0,
     salePrice_lte: 0
@@ -27,16 +48,16 @@ const FilterByPrice = ({ onChange }) => {
   }
 
   return (
-    <Box>
-      <Typography variant='subtitle2'>GIÁ</Typography>
+    <Box className={classes.root}>
+      <Typography variant='subtitle2'>CHỌN KHOẢNG GIÁ</Typography>
 
-      <Box>
+      <Box className={classes.range}>
         <TextField name='salePrice_gte' value={values.salePrice_gte} onChange={handleChange} />
         <span>-</span>
         <TextField name='salePrice_lte' value={values.salePrice_lte} onChange={handleChange} />
       </Box>
 
-      <Button variant='outlined' color='primary' onClick={handleSubmit}>
+      <Button variant='outlined' color='primary' size='small' onClick={handleSubmit}>
         Áp Dụng
       </Button>
     </Box>
