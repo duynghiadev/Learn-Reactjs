a5fca4c4b0aa11de1e153736cc772456fd562a37
duynghiadev <duynghia22302@gmail.com>
2024-03-12 18:22:29 +0700

    Typescript: 02-01 Static type checking là gì? 🤔

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/section-19-react-2022-update/src/learn-typescript-easy/src/lectures/02-03-main.ts
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/section-19-react-2022-update/src/learn-typescript-easy/src/lectures/02-03-main.ts
@@ -1,21 +1,21 @@
-interface Student {
-  id: number
-  name: string
-  gender: 'male' | 'female'
-  age: number
+interface Student_1 {
+  id_1: number
+  name_1: string
+  gender_1: 'male' | 'female'
+  age_1: number
 }
 
-function findStudentById(studentList: Student[], studentId: number): Student | undefined {
+function findStudentById(studentList: Student_1[], studentId: number): Student_1 | undefined {
   if (studentList.length === 0) return undefined
-  return studentList.find((x) => x.id === studentId)
+  return studentList.find((x) => x.id_1 === studentId)
 }
 
-const studentList: Student[] = [
-  { id: 1, name: 'Alice', gender: 'female', age: 20 },
-  { id: 2, name: 'Bob', gender: 'male', age: 20 },
+const studentList_1: Student_1[] = [
+  { id_1: 1, name_1: 'Alice', gender_1: 'female', age_1: 20 },
+  { id_1: 2, name_1: 'Bob', gender_1: 'male', age_1: 20 },
 ]
 
-const bob = findStudentById(studentList, 3)
+const bob = findStudentById(studentList_1, 3)
 
 // console.log(bob.name) // error here ❌
 
@@ -25,7 +25,7 @@ const bob = findStudentById(studentList, 3)
 // FIX: do check before using ✅
 
 if (bob) {
-  console.log(`${bob.name}, ${bob.gender}, ${bob.age}`)
+  console.log(`${bob.name_1}, ${bob.gender_1}, ${bob.age_1}`)
 } else {
   console.log('Student undefined')
 }
---
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/section-19-react-2022-update/src/learn-typescript-easy/src/lectures/studentList.ts
@@ -0,0 +1,4 @@
+const studentList: Student[] = [
+  { id: 1, name: 'Alice', gender: 'female', age: 20 },
+  { id: 2, name: 'Bob', gender: 'male', age: 20 },
+]
