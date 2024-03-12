35c064a050b88e137473196fecf2632ff61e1eb0
duynghiadev <duynghia22302@gmail.com>
2024-03-12 10:01:41 +0700

    ReactJS: 17-11 An toàn render dữ liệu HTML lên UI

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Product/components/ProductDescription.jsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/Route In React/src/features/Product/components/ProductDescription.jsx
@@ -1,5 +1,23 @@
+import { Paper } from '@material-ui/core'
+import DOMPurify from 'dompurify'
+import PropTypes from 'prop-types'
+
 const ProductDescription = ({ product = {} }) => {
-  return <div>{product.description}</div>
+  const safeDescription = DOMPurify.sanitize(product.description)
+
+  return (
+    <Paper elevation={0} style={{ padding: '15px' }}>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: safeDescription
+        }}
+      />
+    </Paper>
+  )
+}
+
+ProductDescription.propTypes = {
+  product: PropTypes.object
 }
 
 export default ProductDescription
