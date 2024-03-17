bcbc2439ef5c62fb2c52f37df7daac57f722a933
duynghiadev <duynghia22302@gmail.com>
2024-03-17 21:10:39 +0700

    176. Do Not Mutate Props

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/section-19-react-2022-update/src/features/labs/Student.tsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/section-19-react-2022-update/src/features/labs/Student.tsx
@@ -4,11 +4,23 @@ export interface StudentCardProps {
   student: Student
 }
 
+// Props are READ ONLY
+// DO NOT MUTATE Props
+// Props are immutable
+
 export function StudentCard({ student }: StudentCardProps) {
-  const { name, isHero, age, hobbyList } = student
+  let { name, isHero, age, hobbyList } = student
+
+  // name = 'Bob'
+  function handleClick() {
+    student.name = 'Bob'
+    console.log(student)
+    // - not trigger re-render
+    // inconsistent data
+  }
 
   return (
-    <div>
+    <div onClick={handleClick}>
       <p>Student: {name}</p>
       <p>Age: {age}</p>
       <p>Hero: {isHero ? 'hero' : 'no-hero'}</p>
