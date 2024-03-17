edbaa0508358e036bcb2800fcf18b7ce96512d11
duynghiadev <duynghia22302@gmail.com>
2024-03-17 16:50:23 +0700

    173. Thực hành Props P1

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/section-19-react-2022-update/src/App.tsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/section-19-react-2022-update/src/App.tsx
@@ -2,26 +2,20 @@ import './App.css'
 import { Footer, Header } from './components/common'
 import Student from './features/labs/Student'
 
-export interface Student {
-  name: string
-  age: number
-  gender: string
-  address?: string
-}
-
 const App = () => {
-  const student: Student = {
-    name: 'John Doe',
-    age: 20,
-    gender: 'male',
-    address: '123 Main Street'
-  }
+  function abc() {}
 
   return (
     <div>
       <Header></Header>
+      <Student
+        name='DuyNghiaDev'
+        age={18}
+        isHero
+        hobbyList={['eat', ' code', ' sleep']}
+        sayHello={abc}
+      />
       <Footer></Footer>
-      <Student student={student} />
     </div>
   )
 }
--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/section-19-react-2022-update/src/features/labs/Student.tsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/section-19-react-2022-update/src/features/labs/Student.tsx
@@ -1,16 +1,23 @@
-import { Student } from '../../App'
-
-interface StudentProps {
-  student: Student
+export interface StudentProps {
+  name?: string
+  age: number
+  isHero: boolean
+  hobbyList: string[]
+  sayHello: () => void
 }
 
-export default function Student({ student }: StudentProps) {
+export default function Student({
+  name = 'Default Name',
+  isHero = false,
+  hobbyList,
+  age
+}: StudentProps) {
   return (
     <div>
-      <p>Name: {student.name}</p>
-      <p>Age: {student.age}</p>
-      <p>Gender: {student.gender}</p>
-      {student.address && <p>Address: {student.address}</p>}
+      <p>Student: {name}</p>
+      <p>Age: {age}</p>
+      <p>Hero: {isHero ? 'hero' : 'no-hero'}</p>
+      <p>Hobby: {hobbyList}</p>
     </div>
   )
 }
