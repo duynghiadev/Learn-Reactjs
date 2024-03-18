587fb5b00ca313ca94a54c5136e806973e353dfe
duynghiadev <duynghia22302@gmail.com>
2024-03-18 12:47:04 +0700

    189. setState is async

--- learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/section-19-react-2022-update/src/App.tsx
+++ learn-reactjs-hau-nguyen-v2/src/Udemy React 2020/src/components/section-19-react-2022-update/src/App.tsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import './App.css'
 
 const App = () => {
+  let i = 0
+
   const [showMore, setShowMore] = useState(false)
   const [count, setCount] = useState(0)
   const [name, setName] = useState('')
 
   const handleIncreaseClick = () => {
-    setCount(count + 1)
+    setCount(count + 1) // async func
+    console.log('after setting count:', count) // old count
+    i += 5
   }
+
+  console.log({ count, i })
 
   return (
     <div>
